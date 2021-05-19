ec832d1cb7d566caf11634520734e32c904279bd
Caitlin Bopp <boppcaitlin@gmail.com>
2021-05-18 21:55:01 -0400

    Added Tag data to include function in both router.get methods in the product-routes.js file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,14 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category, Tag }]
+      include: [
+        {
+          model: Category
+        },
+        {
+          model: Tag
+        }
+      ]
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -23,10 +30,17 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category, Tag }]
+      include: [
+        {
+          model: Category
+        },
+        {
+          model: Tag
+        }
+      ]
     });
 
-    if (!categoryData) {
+    if (!productData) {
       res.status(404).json({ message: 'No product found with this id!' });
       return;
     }
